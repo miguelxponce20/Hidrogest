23a2db165db3ad58bd01d1e369f14d6001470340
endyths <endythsxd@gmail.com>
2025-08-27 21:54:43 -0400

    Vistas admin y mapa

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor de Backend corriendo en el puerto ${PORT}`);
 });
